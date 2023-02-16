6f340bc97bee049ee23f1ba6bb7c418362e5936a
Karol Lima <109041278+daenerys98@users.noreply.github.com>
2023-02-16 20:19:27 -0300

    Add files via upload

---
+++ agora.js
@@ -0,0 +1,17 @@
+function carregar (){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        img.src= "manha.png"
+        document.body.style.background = "#FFFF00"
+    } else if (hora >= 12 && hora < 18){
+        img.src="tarde.png"
+        document.body.style.background = "#FF6347"
+    } else {
+        img.src="noite.png"
+        document.body.style.background = "#D8BFD8"
+    }
+    }
